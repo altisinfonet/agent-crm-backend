ebd75fcff813115c6a8ef01c9e949743ec3cc04a
user6 <user6@altisinfonet.in>
2026-03-10 13:08:05 +0530

    fix

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -13,8 +13,6 @@ export class MailService {
     async sendOTPEmail(subject: string, email: string, otp: string) {
         try {
             const logo = `${process.env.BASE_PATH}/${process.env.IMAGE_PATH}/logo/FinMitraLogo.png`
-            console.log("logo+++++++++++++", logo);
-
             const mailOptions = {
                 to: email,
                 subject: subject,
