f8209b0082a1e76894cf92a53ae1529acfe0c910
user4 <user4@altisinfonet.in>
2026-03-13 11:43:21 +0530

    up

--- src/common/redis/redis.client.ts
+++ src/common/redis/redis.client.ts
@@ -1,7 +1,9 @@
 import Redis from "ioredis";
 
 export const redis = new Redis({
-    host: "127.0.0.1",
+    host: process.env.REDIS_HOST || "127.0.0.1",
     port: Number(process.env.REDIS_PORT) || 6379,
-    // port: 6379,
+    ...(process.env.REDIS_USERNAME && { username: process.env.REDIS_USERNAME }),
+    ...(process.env.REDIS_PASSWORD && { password: process.env.REDIS_PASSWORD }),
+    ...(process.env.REDIS_DB && { db: Number(process.env.REDIS_DB) }),
 });
